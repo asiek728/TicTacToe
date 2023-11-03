a0c19245366cfda4d45bc0080fbc370c7bd72540
asiek728 <asiek728@gmail.com>
2023-11-03 12:55:45 +0000

    computer mode added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const prompt = require('prompt-sync')({ sigint: true });
 const c = require('ansi-colors');
 const { printBoard } = require('./printBoard');
 const { checkPos } = require('./checkPos');
+const { checkPosComp } = require('./checkPosComp');
 const { checkVictory } = require('./checkVictory');
 
 let row1 = ['-', '-', '-'];
@@ -22,8 +23,7 @@ console.log(c.yellow('Welcome to the Tic-Tac-Toe game! 👋'));
 
 printBoard(row1, row2, row3);
 
-const startRound = () => {
-
+const startTwoUsersRound = () => {
   while (!hasPlayerWon) {
     if (player1) {
       let input1 = prompt(
@@ -65,9 +65,67 @@ const startRound = () => {
   }
 }
 
+const startOneUserRound = () => {
+  while (!hasPlayerWon) {
+    if (player1) {
+      let input1 = prompt(
+        c.bgMagentaBright('Player 1') +
+        ' => please select the position you want to occupy (x1, x2, x3, y1, y2, y3, z1, z2, z3): '
+      );
 
-const startGame = () => {
-  startRound();
+      const output = checkPos(row1, row2, row3, input1, 'x');
+
+      if (output === undefined) {
+        printBoard(row1, row2, row3);
+        player2 = true;
+        player1 = false;
+
+        if (checkVictory(row1, row2, row3, 'x') === true) {
+          break;
+        }
+      }
+    }
+
+    if (player2) {
+  
+      const validInputs = ['x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x3', 'y3', 'z3'];
+      const random = Math.floor(Math.random() * validInputs.length);
+      const output = checkPosComp(row1, row2, row3, validInputs[random], 'o');
+
+      if (output === undefined) {
+        printBoard(row1, row2, row3);
+        player1 = true;
+        player2 = false;
+
+        if (checkVictory(row1, row2, row3, 'o') === true) {
+          break;
+        }
+      }
+    }
+  }
+}
+
+
+startGame = () => {
+  let input1 = prompt(
+    'How many players will be playing today? (1 or 2) '
+  );
+  if (input1 == 2) {
+    startGameMode2();
+  }
+  else if (input1 == 1) {
+    startGameMode1();
+  }
+  else {
+    console.log('Please enter 1 or 2')
+    startGame();
+  }
+
+}
+
+const startGameMode2 = () => {
+
+  startTwoUsersRound();
   let input = 'y';
   while (input.toLowerCase() !== 'n') {
     input = prompt(
@@ -79,7 +137,7 @@ const startGame = () => {
       player2 = false;
       clearBoard();
       printBoard(row1, row2, row3);
-      startRound();
+      startTwoUsersRound();
     }
     else if (input.toLowerCase() === 'n') {
       console.log('Thanks for playing! 👋')
@@ -89,5 +147,36 @@ const startGame = () => {
     }
   }
 }
+
+
+
+
+const startGameMode1 = () => {
+
+  startOneUserRound();
+  let input = 'y';
+  while (input.toLowerCase() !== 'n') {
+    input = prompt(
+      'Do you want to start a new game? (y or n) '
+    );
+
+    if (input.toLowerCase() === 'y') {
+      player1 = true;
+      player2 = false;
+      clearBoard();
+      printBoard(row1, row2, row3);
+      startOneUserRound();
+    }
+    else if (input.toLowerCase() === 'n') {
+      console.log('Thanks for playing! 👋')
+    }
+    else {
+      console.log('Please enter y or n')
+    }
+  }
+
+}
+
+
 
 startGame();
---
+++ checkPosComp.js
@@ -0,0 +1,71 @@
+const c = require('ansi-colors');
+
+const validInputs = ['x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x3', 'y3', 'z3'];
+
+const checkPosComp = (row1, row2, row3, position, player) => {
+  let inputtedPos = position.toLowerCase();
+  if (!inputtedPos) {
+    console.log(c.red(`\nERROR: you must enter a value!\n`));
+    return false;
+  } else if (!validInputs.includes(inputtedPos)) {
+    console.log(c.red(`\nERROR: position entered is invalid!\n`));
+    return false;
+  } else if (inputtedPos === 'x1') {
+    if (row1[0] === '-') {
+      row1[0] = player;
+    } else {
+      return false;
+    }
+  } else if (inputtedPos === 'x2') {
+    if (row1[1] === '-') {
+      row1[1] = player;
+    } else {
+      return false;
+    }
+  } else if (inputtedPos === 'x3') {
+    if (row1[2] === '-') {
+      row1[2] = player;
+    } else {
+      return false;
+    }
+  } else if (inputtedPos === 'y1') {
+    if (row2[0] === '-') {
+      row2[0] = player;
+    } else {
+      return false;
+    }
+  } else if (inputtedPos === 'y2') {
+    if (row2[1] === '-') {
+      row2[1] = player;
+    } else {
+      return false;
+    }
+  } else if (inputtedPos === 'y3') {
+    if (row2[2] === '-') {
+      row2[2] = player;
+    } else {
+      return false;
+    }
+  } else if (inputtedPos === 'z1') {
+    if (row3[0] === '-') {
+      row3[0] = player;
+    } else {
+      return false;
+    }
+  } else if (inputtedPos === 'z2') {
+    if (row3[1] === '-') {
+      row3[1] = player;
+    } else {
+      return false;
+    }
+  } else if (inputtedPos === 'z3') {
+    if (row3[2] === '-') {
+      row3[2] = player;
+    } else {
+      return false;
+    }
+  }
+  !inputtedPos;
+};
+
+module.exports = { checkPosComp };
